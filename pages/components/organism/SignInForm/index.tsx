d628007b536f393bc3738fed9632f87fee473948
irmawan <irmawan9001@gmail.com>
2022-04-20 00:38:52 +0700

    signin page finish

--- pages/components/organism/SignInForm/index.tsx
+++ pages/components/organism/SignInForm/index.tsx
@@ -1,8 +1,35 @@
 import { useState } from "react";
+import Link from "next/link";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { setLogin } from "../../../../services/auth";
+import { useRouter } from "next/router";
+import { route } from "next/dist/server/router";
 
 export default function SignInForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    const data = {
+      email,
+      password,
+    };
+    console.log("data : ", data);
+    if (!email || !password) {
+      toast.error("Email dan Password Wajib diisi");
+    } else {
+      const response = await setLogin(data);
+      if (response.error) {
+        toast.error(response.message);
+      } else {
+        toast.success("Login Berhasil");
+        router.push("/");
+      }
+    }
+  };
   return (
     <>
       <h2 className="text-4xl fw-bold color-palette-1 mb-10">Sign In</h2>
@@ -25,10 +52,7 @@ export default function SignInForm() {
         />
       </div>
       <div className="pt-30">
-        <label
-          htmlFor="password"
-          className="form-label text-lg fw-medium color-palette-1 mb-10"
-        >
+        <label className="form-label text-lg fw-medium color-palette-1 mb-10">
           Password
         </label>
         <input
@@ -38,24 +62,25 @@ export default function SignInForm() {
           name="password"
           aria-describedby="password"
           placeholder="Your password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="button-group d-flex flex-column mx-auto pt-50">
-        <a
+        <button
+          type="button"
           className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-          href="/"
-          role="button"
+          onClick={onSubmit}
         >
           Continue to Sign In
-        </a>
-        <a
-          className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
-          href="/sign-up"
-          role="button"
-        >
-          Sign Up
-        </a>
+        </button>
+        <Link href="/sign-up">
+          <a className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill">
+            Sign Up
+          </a>
+        </Link>
       </div>
+      <ToastContainer />
     </>
   );
 }
